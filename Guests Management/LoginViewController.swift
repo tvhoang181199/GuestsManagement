c4ba5068e1b2bce1c7760d42544dca8a9e664f9c
vhoang <vhtrinh73@gmail.com>
2019-11-14 00:14:49 +0700

    add login api to test api

--- Guests Management/LoginViewController.swift
+++ Guests Management/LoginViewController.swift
@@ -6,17 +6,76 @@
 //  Copyright © 2019 Vũ Hoàng Trịnh. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import SCLAlertView
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var _username: UITextField!
+    @IBOutlet weak var _password: UITextField!
+    
+    var username: String = ""
+    var password: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func loginButton(_ sender: Any) {
+        let url = URL(string: "http://private-908781-loginapi63.apiary-mock.com/login")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
 
+        let json: [String: Any] = ["login": "request"]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        request.httpBody = jsonData
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let dictArray = responseJSON as? [String: Any] {
+                for item in dictArray {
+                    self.username = item.key
+                    self.password = item.value as! String
+                }
+                DispatchQueue.main.async(execute: self.doLogin)
+            }
+        }
+
+        task.resume()
+    }
+    
+    func doLogin() {
+        let appearance = SCLAlertView.SCLAppearance(
+            showCloseButton: false
+        )
+        let alert = SCLAlertView(appearance: appearance)
+        alert.addButton("OK") { () -> Void in
+            let story = self.storyboard
+            let vc = story?.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true)
+        }
+        
+        if (_username.text == username && _password.text == password) {
+            alert.showSuccess("Success", subTitle: "Login successfully")
+        }
+        else {
+            SCLAlertView().showError("Failed", subTitle: "Your username or password is invalid!!!")
+        }
+        
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        _username.text = ""
+        _password.text = ""
+    }
     /*
     // MARK: - Navigation
 
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 	1. Open terminal and change directory to this one
 	2. Install pods with the command: pod install
 	3. Open Guests Management.xcworkspace: open Guests Management.xcworkspace
+	4. Login API test with default account (username: username, password: pass)
